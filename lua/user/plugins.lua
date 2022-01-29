e7600a2d624a55dce648a48f68ba69f556c8f7c9
U-L-M-S <uinclis@outlook.de>
2022-01-29 14:33:55 +0100

    rename debbuged BUT lsp server can not install languages(it install but commands like PRINT does not work) Test with another servers instead of pyright

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,13 +61,16 @@ return packer.startup(function(use)
   use "andweeb/presence.nvim"
   use "ethanholz/nvim-lastplace"
   -- For renamer
-  use {
-    "filipdutescu/renamer.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("user/rename").setup()
-    end,
-  }
+--  use {
+--   "filipdutescu/renamer.nvim",
+--    requires = { "nvim-lua/plenary.nvim" },
+--  config = function()
+--      require("user/rename").setup()
+--  end,
+--  }
+use "nathom/easy-replace.nvim"
+
+
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
