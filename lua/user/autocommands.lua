3580ba133f4fd2726722508f66dfdb8051a526f5
U-L-M-S <uinclis@outlook.de>
2022-01-29 20:05:31 +0100

    set commands to run code inside nvim

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,17 +28,11 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-  " augroup CBuild
-  "   autocmd! filetype c        nnoremap <leader>zc :!gcc -o %:p:r %<cr>
-  "   autocmd! filetype c        nnoremap <leader>zr :!gcc -o %:p:r %<cr>:!%:p:r<cr>
-  "   autocmd! filetype cpp      nnoremap <leader>zc :!g++ -o %:p:r %<cr>
-  "   autocmd! filetype cpp      nnoremap <leader>zr :!g++ -o %:p:r %<cr>:!%:p:r<cr>
-  " augroup END
-  "
-" To run C/C++ inside nvim
+
+"To run C/C++ inside nvim
   map <F4> :w <CR> :!gcc % -o %< && ./%< <CR>
-  " imap <F4> :w <CR> :!gcc % -o %< && ./%< <CR>
-" To run python inside nvim
+  "imap <F4> :w <CR> :!gcc % -o %< && ./%< <CR>
+"To run python inside nvim
   autocmd FileType python map <buffer> <F5> :w<CR>:exec '!python3' shellescape(@%, 1)<CR>
   autocmd FileType python imap <buffer> <F5> <esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>
 ]]
